58eaedba69662f0a3695852b8a7c4e4fdc0874ab
ben <ben99s@gmail.com>
2018-09-20 08:00:17 -0400

    Adding new assignment, cstrings

---
+++ Assignment6_C-Strings/Assignment6_C-Strings/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+
+int findW(char[], int);
+long getCustNum(char[], int);
+int getYear(char[], int);
+long getOrderNum(char[], int, int);
+
+int main() {
+	const int SIZE = 20; //Char Array Size
+	char custNumber[6], year[2], workOrderNumber[5]; //Data to be parse from workOrder string
+	char workOrder[13] = "91800w940770"; //String to parse
+
+
+
+
+	return 0;
+}
