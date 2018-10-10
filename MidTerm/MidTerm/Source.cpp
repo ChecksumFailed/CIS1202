7ca2884811a69e4f6f6e9dec663df8efd8e809d5
ben <ben99s@gmail.com>
2018-10-10 13:02:35 -0400

    Add functions

--- MidTerm/MidTerm/Source.cpp
+++ MidTerm/MidTerm/Source.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 //Global Constants
 const int NUMFIELDS = 4; //Number of fields stored in 2d array
+const int MAXMOVIES = 500; //Maximum size of database
 /*
 0 - Title
 1 - Year
@@ -22,7 +23,7 @@ const int NUMFIELDS = 4; //Number of fields stored in 2d array
 
 //Functions
 void deleteMovie(string[][NUMFIELDS], int, int &); //Delete movie from array
-void loadArrays(string arr[][NUMFIELDS], int &, string); //Read in file and load array
+void loadDB(string arr[][NUMFIELDS], int &, string); //Read in file and load array
 vector<string> parseString(string , char ); //Split delimted string
 string getString(); //Get string input and validate
 string getString(string); //get string input and validate.  Pass string to match for validation
@@ -31,20 +32,26 @@ int getInt(int, int); //get int input and validate range.
 void displayMenu(); //display main menu
 void addMovie(string *, int&); //Add movie
 void printMovies(string *,int); //print all movies in array
-
+void searchMenu(string[][NUMFIELDS], int); //Generates menu for searching the 2d array
+void updateDB(string*, string, int); //Writes changes to disk.
+vector<string> searchMovies(string[][NUMFIELDS], int, int);
 
 /*
 Programmer Name: Ben Scherer
 Program # : 3
 Course: CIS 1202 Section 502
 Date : 10/7/2018
-Program Description : Mid Term prooject.  Movie database.  Supports CRUD operations and peristent changes.
+Program Description : Mid Term prooject.  Movie database.  Supports CRUD operations and peristent changes.  Heavily leverages pointers and 2D arrays per requirements
 */
 int main() {
-	string movies[500][NUMFIELDS]; //2d array to store movie details.  
+	string movies[MAXMOVIES][NUMFIELDS]; //2d array to store movie details.  
 	int size = 0; //current size of array
 	int choice; //stores user input
+	string dbFile = "movieDB.dat";
 	 
+
+	loadDB(movies, size, dbFile);
+	//Load existing movies into array
 
 
 	//Loop until user exits
@@ -70,6 +77,8 @@ int main() {
 		
 	} while (choice != 4);
 
+	cout << "Writing changes to disk....";
+	updateDB(&movies[0][0], dbFile, size);
 
 	//pauses program
 	cout << "Press enter key to exit program\n";
@@ -78,14 +87,41 @@ int main() {
 }
 
 
+void searchMenu(string movies[][NUMFIELDS], int size) {
+	int choice;
+	cout << "\n 1 - Search by Title\n"
+		<< "2 - Search by Year\n"
+		<< "3 - Search by Genre\n"
+		<< "4 - Search by Rating\n"
+		<< "5 - Quit\n"
+		<< "Enter Choice: ";
 
+	do {
+		choice = getInt();
+		if (choice >= 1 && choice < 5)
+			searchMovies(movies, size, choice);
+		else if (choice == 5)
+			return;
+		else
+			cout << "Invalid Choice.  Try again: ";
+
+
+	} while (choice != 5);
+
+}
+
+vector<string> searchMovies(string movies[][NUMFIELDS],int size,int choice) {
+	vector<string> searchResult;
+	return searchResult;;
+}
 void printMovies(string *arr, int size) {
 	int counter = 0;
 
 
-	cout << setw(20) << "Title" << setw(20) << "Year" << setw(20) << "Genre" << setw(20) << "Rating" << endl;
+	cout << left << setw(20) << "Title" << setw(20) << "Year" << setw(20) << "Genre" << setw(20) << "Rating" << endl;
 
 	for (int i = 0; i < size; i++) {
+		cout << left;
 		for (int j = 0; j < NUMFIELDS; j++) {
 			cout << setw(20) << *(arr+ (i*NUMFIELDS) + j);
 		}
@@ -99,6 +135,11 @@ void printMovies(string *arr, int size) {
 }
 
 void addMovie(string *arr, int &size) {
+	if (size == MAXMOVIES) {
+		cout << "The current release has a limitation of : " << MAXMOVIES << ".  Delete entries prior to adding more.";
+		return;
+	}
+
 	cout << size << endl;
 	cout << "Enter title of movie: ";
 	*arr = getString();;
@@ -219,7 +260,7 @@ void deleteMovie(int arr[][NUMFIELDS], int index, int &size) {
 
 }
 
-void loadArrays(string arr[][NUMFIELDS], int &size, string filePath) {
+void loadDB (string arr[][NUMFIELDS], int &size, string filePath) {
 	string strHold; //placeholder for getLine
 	ifstream fileStream(filePath); //input stream for stats file
 	
@@ -273,4 +314,19 @@ vector<string> parseString(string strToSplit, char delim) {
 	}
 
 	return parsedItems;
+}
+
+
+//write changes to db file
+void updateDB(string *arr, string file, int size) {
+	ofstream outFile(file);
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < NUMFIELDS; j++) {
+			outFile << *(arr + (i*NUMFIELDS) + j);
+			if (i != NUMFIELDS - 1)
+				outFile << "|";
+		}
+		outFile << endl;
+	}
+	outFile.close();
 }
